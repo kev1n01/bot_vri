fae7ed4c01e3bc31aba196d23777a7f8d29736cc
kevin arnold flores pacheco <arnold_k2001@hotmail.com>
2024-08-29 13:08:09 -0500

    send hola into atention hor

--- src/flows/options/soporteFlow.ts
+++ src/flows/options/soporteFlow.ts
@@ -7,7 +7,7 @@ export const soporteFlow = addKeyword(EVENTS.ACTION)
         console.log(time);
         console.log(time <= '07:59:00' || time >= '12:00:00');
         if (time <= '07:59:00' || time >= '13:00:00') {
-            return endFlow(`En estos momentos el soporte no está disponibilida, por estar fuera de horario de atención (Lunes a viernes de 8am - 1pm y de 3pm - 6pm)`)
+            return endFlow(`En estos momentos el soporte no está disponibilida, por estar fuera de horario de atención (Lunes a viernes de 8am - 1pm y de 3pm - 6pm), envienos un "*Hola*" más tarde`)
         }
 
         try {
